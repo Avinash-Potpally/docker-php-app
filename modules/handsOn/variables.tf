1fe1dd9c871695b7c3e5e9ae2440c84b5826e7c2
Avinash Kumar P <Avinash-Potpally@users.noreply.github.com>
2024-08-19 18:14:53 +0530

    Update variables.tf

--- modules/handsOn/variables.tf
+++ modules/handsOn/variables.tf
@@ -24,3 +24,23 @@ variable "tags" {
   }
 
 }
+variable "client_id" {
+  description = "The client ID of the Service Principal."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The client secret of the Service Principal."
+  type        = string
+  sensitive   = true
+}
+
+variable "subscription_id" {
+  description = "The subscription ID."
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant ID."
+  type        = string
+}
